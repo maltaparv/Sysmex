48107490f7f8ae065b93e3426c7f5a34628540a7
Maltapar <maltapar@gmail.com>
2020-12-01 17:09:46 +0300

    Sample_gui_client + scket_clietn_Thread+test_serv

--- 2exe.cmd
+++ 2exe.cmd
@@ -1,6 +1,10 @@
 rem яЁютхЁър эр яхЁхфрээ√х ярЁрьхЄЁ√:
 if x%1==x goto Err01
+
 set fname=%1
+set fn=%fname:~0,-3%
+rem fn - если в качестве параметра ввидить имя файла с расширением .py - три последних символа отсекаются.
+
 set PathTo=%2
 set ArcName=%3
 
@@ -10,10 +14,12 @@ set mm=%da:~3,2%
 set gg=%da:~6,4%
 set ArcName=%3%dd%
 
-pyinstaller --onedir --onefile --name=%1 %1.py 
+echo pyinstaller --onedir --onefile --name=%1 %1.py 
+rem pyinstaller --onedir --onefile --name=%fn% %fn%.py
+pyinstaller --onedir --onefile --name=%fn% -i "C:\DRV\Sysmex550\sysmex550.ico" %fn%.py
 
 rem с иконкой:
-rem pyinstaller --onedir --onefile --name=%1 %1.py -i "C:\Data\Icon.ico 
+rem pyinstaller --onedir --onefile --name=%1 -i "C:\Data\Icon.ico" %1.py
 exit
 
 :Err01
--- Sysmex_GUI.py
+++ Sysmex_GUI.py
@@ -1,6 +1,16 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 from mydesign import Ui_MainWindow, QtGui, QtCore  # импорт нашего сгенерированного файла
 import sys
+# doc: https://python-scripts.com/pyqt5#pyqt5-designer
+import socket
+import pyodbc  # для MS SQL SERVER - рекомендовано Microsoft
+from Parsing import parse_xn350, record
+from ProcLib import write_log, write_errlog, read_ini
+from ClassLib import const
+from Sysmex_XN import create_socket, sql_insert, transfer, mainloop
+from threading import Thread
+#import _thread
 
 
 class mywindow(QtWidgets.QMainWindow):
@@ -11,14 +21,49 @@ class mywindow(QtWidgets.QMainWindow):
         # self.ui.label.setFont(QtGui.QFont('SansSerif', 30))  # Изменение шрифта и размера
         # self.ui.label.setGeometry(QtCore.QRect(10, 10, 200, 200))  # изменить геометрию ярлыка
         # self.ui.btn_run.clicked.connect(self.btnClicked)  # подключение клик-сигнал к слоту btnClicked
-        # self.ui.btn_run.clicked.connect(btn_click)  # подключение клик-сигнал к слоту btnClicked
-
+        self.ui.btn_run.clicked.connect(btn_click)  # подключение клик-сигнал к слоту btnClicked
+        self.ui.btn_test_1.clicked.connect(btn_test1)
+        self.ui.btn_test_2.clicked.connect(btn_test2)
 
 def btn_click():
-    print('Click!')
+    msg = 'Старт прослушки порта. '
+    print(msg)
+    application.ui.plainTextEdit.appendPlainText(msg)
+    # thread1 = Thread(target=mainloop())
+    # thread1.start()
+    # _thread.start_new_thread(mainloop())
+    # mainloop()
+
+    daemonThread.start()
+
+def btn_test1():
+    print('test1')
+    application.ui.plainTextEdit.appendPlainText('test1')
 
 
-app = QtWidgets.QApplication([])
-application = mywindow()
-application.show()
-sys.exit(app.exec())
+def btn_test2():
+    msg = 'закрыть прослушку порта. '
+    print(msg)
+    application.ui.plainTextEdit.appendPlainText(msg)
+    #thread1.join()
+    daemonThread.join()
+
+
+if __name__ == '__main__':
+    # ToDo_done read_ini
+    # const = CONST()  # to fill from ini
+    # fn_ini = 'sysmex350.ini'
+    fn_ini = 'sysmex.ini'
+    read_ini(fn_ini)
+    write_log(f'Run {const.analyser_name}, analyser_id={const.analyser_id}, ' +
+              f'analyser_location={const.analyser_location}, listening IP:{const.host}:{const.port}.')
+
+
+    app = QtWidgets.QApplication([])
+    application = mywindow()
+    application.show()
+
+    daemonThread = Thread(target=mainloop())
+    daemonThread.setDaemon(True)
+
+    sys.exit(app.exec())
